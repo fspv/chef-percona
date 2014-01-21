e4828b375b73277017acbe8a7a7c8cccea7c974b
Pavel Safronov <prius@yandex-team.ru>
2014-01-21 22:53:31 +0400

    Fixed always creating grant for REPLICATION SLAVE

--- providers/grant.rb
+++ providers/grant.rb
@@ -1,8 +1,8 @@
 action :create do
   # Create grants if they are not exists already
   execute "#{new_resource.user}@#{new_resource.host}: GRANT #{new_resource.privileges}" do
-    command 'mysql -e "GRANT ' + new_resource.privileges + " ON " + new_resource.database + "." + new_resource.table + " TO '" + new_resource.user + "'@'" + new_resource.host + "' " + new_resource.with_option + ';"'
-    not_if 'mysql -r -B -e "SHOW GRANTS FOR \'' + new_resource.user + '\'@\'' + new_resource.host + '\';" | grep -F "GRANT ALL PRIVILEGES ON \`' + new_resource.database + '\`.' + new_resource.table + " TO '" + new_resource.user + "'@'" + new_resource.host + "'\""
+    command "mysql -e \"GRANT #{new_resource.privileges}  ON #{new_resource.database}.#{new_resource.table} TO '#{new_resource.user}'@'#{new_resource.host}' #{new_resource.with_option};\""
+    not_if "mysql -r -B -e \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}'\" | grep -q \"GRANT #{new_resource.privileges} ON #{new_resource.database}.#{new_resource.table} TO '#{new_resource.user}'@'#{new_resource.host}'\""
   end
 end
 
@@ -10,6 +10,6 @@ action :delete do
   # Revoke grants if they are exists
   execute "#{new_resource.user}@#{new_resource.host}: REVOKE #{new_resource.privileges}" do
     command 'mysql -e "REVOKE ' + new_resource.privileges + " ON " + new_resource.database + "." + new_resource.table + " FROM '" + new_resource.user + "'@'" + new_resource.host + "';\""
-    only_if 'mysql -r -B -e "SHOW GRANTS FOR \'' + new_resource.user + '\'@\'' + new_resource.host + '\';" | grep -F "GRANT ALL PRIVILEGES ON \`' + new_resource.database + '\`.' + new_resource.table + " TO '" + new_resource.user + "'@'" + new_resource.host + "'\""
+    only_if "mysql -r -B -e \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}'\" | grep -q \"GRANT #{new_resource.privileges} ON #{new_resource.database}.#{new_resource.table} TO '#{new_resource.user}'@'#{new_resource.host}'\""
   end
 end
